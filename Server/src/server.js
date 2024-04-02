83cdf1465a37c30e8c48fb4d9daa9029a99c8b77
jakeladdd22 <111535028+jakeladdd22@users.noreply.github.com>
2024-04-02 01:11:21 +0100

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,60 +1,63 @@
 // Made by Mika (mikayla.js) - No credits needed, selling/distribution is not permitted
 // https://github.com/pastrified
 
-const express = require('express');
-const CryptoJS = require('crypto-js');
+const express = require("express");
+const CryptoJS = require("crypto-js");
 const app = express();
-const http = require('http');
-const socketIo = require('socket.io');
+const http = require("http");
+const socketIo = require("socket.io");
 const server = http.createServer(app);
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const io = socketIo(server, {
-  transports: ['websocket'],
+  transports: ["websocket"],
 });
 
-const mongoose = require('mongoose');
-const {Schema} = require('mongoose');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
-const axios = require('axios');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 app.use(express.json());
 
 app.use(cors());
 
-mongoose.connect('inputstringhere', { useNewUrlParser: true }) // add info
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to database.'));
+mongoose.connect(
+  'mongodb+srv://cluster0.vlskhs8.mongodb.net/" --apiVersion 1 --username Jacob',
+  { useNewUrlParser: true },
+); // add info
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database."));
 
 const mmIdentify = new mongoose.Schema({
-    crypto: { type: String, required: true },
-    identifier: { type: String, required: true },
-    address: { type: String, required: true, default: "0" },
-    status: { type: String, required: true, default: "0" },
-    channelId: {type: String, required: true, default: "0"},
-    sender: { type: String, required: true, default: "0"},
-    receiver: { type: String, required: true, default: "0"},
-    creatorUserId: {type: String, required: true, default: "0"},
-    otherUserId: {type: String, required: true, default: "0"},
-    amountInUsd: { type: Number, required: true, default: "0"},
-    amountInCrypto: {type: Number, required: true, default: "0"},
-    createdAt: { type: Date, required: true }
-})
+  crypto: { type: String, required: true },
+  identifier: { type: String, required: true },
+  address: { type: String, required: true, default: "0" },
+  status: { type: String, required: true, default: "0" },
+  channelId: { type: String, required: true, default: "0" },
+  sender: { type: String, required: true, default: "0" },
+  receiver: { type: String, required: true, default: "0" },
+  creatorUserId: { type: String, required: true, default: "0" },
+  otherUserId: { type: String, required: true, default: "0" },
+  amountInUsd: { type: Number, required: true, default: "0" },
+  amountInCrypto: { type: Number, required: true, default: "0" },
+  createdAt: { type: Date, required: true },
+});
 
 const ticketSchema = new mongoose.Schema({
-    category: {
-      type: String,
-      required: true,
-      enum: ['btc', 'ltc', 'eth', 'doge', 'usdt'],
-    },
-    ticketCount: {
-      type: Number,
-      default: 0,
-    },
-  });
+  category: {
+    type: String,
+    required: true,
+    enum: ["btc", "ltc", "eth", "doge", "usdt"],
+  },
+  ticketCount: {
+    type: Number,
+    default: 0,
+  },
+});
 
 const confirmationOne = new mongoose.Schema({
   channelId: {
@@ -79,77 +82,79 @@ const mmPayment = new mongoose.Schema({
   address: { type: String, required: true },
   orderId: { type: String, required: true },
   trackId: { type: String, required: true },
-  channelId: {type: String, required: true },
+  channelId: { type: String, required: true },
   sender: { type: String, required: true },
-})
-  
-const PaymentMM = mongoose.model('mmPayment', mmPayment)
-const ConfirmationOne = mongoose.model('confirmationOne', confirmationOne);
-const Ticket = mongoose.model('ticketCount', ticketSchema);
-const IdentifyMM = mongoose.model('escrowTicket', mmIdentify)
+});
 
-app.post('/utils/oxa-callback', async (req, res)=> {
+const PaymentMM = mongoose.model("mmPayment", mmPayment);
+const ConfirmationOne = mongoose.model("confirmationOne", confirmationOne);
+const Ticket = mongoose.model("ticketCount", ticketSchema);
+const IdentifyMM = mongoose.model("escrowTicket", mmIdentify);
+
+app.post("/utils/oxa-callback", async (req, res) => {
   const status = req.body.status;
   const txId = req.body.txId;
   const trackId = req.body.trackId;
 
-  console.log(req.body)
-  
-  let mmExchanger = await PaymentMM.findOne({ trackId: `${trackId}` })
+  console.log(req.body);
 
-  let confirmaddy = mmExchanger.address
-  let mmExchange = await IdentifyMM.findOne({ address: `${confirmaddy}` })
+  let mmExchanger = await PaymentMM.findOne({ trackId: `${trackId}` });
+
+  let confirmaddy = mmExchanger.address;
+  let mmExchange = await IdentifyMM.findOne({ address: `${confirmaddy}` });
 
   if (status.toLowerCase() == "failed") {
     const channelIdFromDB = mmExchange.channelId;
     const errorMessage = "invalid_payment";
 
-    io.emit('payment-error', {
+    io.emit("payment-error", {
       channelId: channelIdFromDB,
-      error: errorMessage
+      error: errorMessage,
     });
 
-    return res.status(200).json({ status: "fail", error: errorMessage});
+    return res.status(200).json({ status: "fail", error: errorMessage });
   } else if (status.toLowerCase() == "expired") {
     const channelIdFromDB = mmExchange.channelId;
     const errorMessage = "payment_expired_no_payment_identified";
 
-    io.emit('payment-error', {
+    io.emit("payment-error", {
       channelId: channelIdFromDB,
-      error: errorMessage
+      error: errorMessage,
     });
 
-    return res.status(200).json({ status: "fail", error: errorMessage});
+    return res.status(200).json({ status: "fail", error: errorMessage });
   } else if (status.toLowerCase() == "confirming") {
     const channelIdFromDB = mmExchange.channelId;
     const Message = "processing_payment";
 
-    io.emit('payment-processing', {
+    io.emit("payment-processing", {
       channelId: channelIdFromDB,
       message: Message,
       txId: txId,
     });
 
-    return res.status(200).json({ status: "processing", message: Message});
+    return res.status(200).json({ status: "processing", message: Message });
   } else if (status.toLowerCase() == "paid") {
     const channelIdFromDB = mmExchange.channelId;
     const Message = "payment_made";
 
-    io.emit('payment-confirmed', {
+    io.emit("payment-confirmed", {
       channelId: channelIdFromDB,
       message: Message,
       txId: txId,
     });
 
-    return res.status(200).json({ status: "confirmed", message: Message});
+    return res.status(200).json({ status: "confirmed", message: Message });
   }
 });
 
-app.get('/', async (req, res) => {
-  res.status(200).json({ status: 'success', message: 'Middleman Service - v1.0' });
+app.get("/", async (req, res) => {
+  res
+    .status(200)
+    .json({ status: "success", message: "Middleman Service - v1.0" });
 });
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   console.log(socket.id);
 });
 
